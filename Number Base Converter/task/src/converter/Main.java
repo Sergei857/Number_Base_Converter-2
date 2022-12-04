be149d8869b12b363f262f5af435975498cc2db1
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2022-12-04 19:22:47 +0300

    2.1

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -8,19 +8,127 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
-        System.out.println("Enter number in decimal system:");
-        int decimal_input = Integer.parseInt(InputStr());
-        System.out.println("Enter target base:");
-        int target_sys_input = Integer.parseInt(InputStr());
-        //System.out.println(decimal_input + " -> " + target_sys_input);
-        System.out.println("Conversion result:");
-
+        while (true) {
+            String out = "";
+            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            String type_to = InputStr();
+            if (type_to.equals("/exit")) break;
+            switch (type_to) {
+                case "/from" -> {
+                    System.out.print("Enter number in decimal system: ");
+                    int decimal_input = Integer.parseInt(InputStr());
+                    System.out.print("Enter target base: ");
+                    int target_sys_input = Integer.parseInt(InputStr());
+                    switch (target_sys_input) {
+                        case 2 -> System.out.println("Conversion result: " + DecimalTo2(decimal_input));
+                        case 8 -> System.out.println("Conversion result: " + DecimalTo8(decimal_input));
+                        case 16 -> System.out.println("Conversion result: " + DecimalTo16(decimal_input));
+                    }
+                }
+                case "/to" -> {
+                    System.out.print("Enter source number: ");
+                    String sourse_input = InputStr();
+                    System.out.print("Enter source base: ");
+                    int target_sys_input = Integer.parseInt(InputStr());
+                    switch (target_sys_input) {
+                        case 2 -> System.out.println("Conversion to decimal result: " + BinToDecimal(sourse_input));
+                        case 8 -> System.out.println("Conversion to decimal result: " + V8ToDecimal(sourse_input));
+                        case 16 -> System.out.println("Conversion to decimal result: " + S16ToDecimal(sourse_input));
+                    }
+                }
+            }
+        }
     }
+
 
     public static String InputStr() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
 
     }
+
+    public static String DecimalTo2(int dec) {
+        Integer remainder, quotient;
+        StringBuilder out_str = new StringBuilder("");
+        while (true) {
+            remainder = dec % 2;
+            quotient = dec / 2;
+            dec = quotient;
+            out_str.append(remainder);
+            if (quotient < 2 & dec != 0) {
+                out_str.append(dec);
+                break;
+            } else if (quotient < 2) break;
+        }
+        return out_str.reverse().toString();
+    }
+
+    public static String DecimalTo8(int dec) {
+        Integer remainder, quotient;
+        StringBuilder out_str = new StringBuilder("");
+        while (true) {
+            remainder = dec % 8;
+            quotient = dec / 8;
+            dec = quotient;
+            out_str.append(remainder);
+            if (quotient < 8 & dec != 0) {
+                out_str.append(dec);
+                break;
+            } else if (quotient < 8) break;
+        }
+        return out_str.reverse().toString();
+    }
+
+    public static String DecimalTo16(int dec) {
+        Integer remainder, quotient;
+        StringBuilder out_str = new StringBuilder("");
+        String remainder_str = "";
+        while (true) {
+            remainder = dec % 16;
+            quotient = dec / 16;
+            dec = quotient;
+            if (remainder < 10) {
+                remainder_str = remainder.toString();
+            } else {
+                switch (remainder) {
+                    case 10 -> remainder_str = "A";
+                    case 11 -> remainder_str = "B";
+                    case 12 -> remainder_str = "C";
+                    case 13 -> remainder_str = "D";
+                    case 14 -> remainder_str = "E";
+                    case 15 -> remainder_str = "F";
+                }
+            }
+            out_str.append(remainder_str);
+            if (quotient < 16 & dec != 0) {
+                remainder = dec;
+                if (remainder < 10) {
+                    remainder_str = remainder.toString();
+                } else {
+                    switch (remainder) {
+                        case 10 -> remainder_str = "A";
+                        case 11 -> remainder_str = "B";
+                        case 12 -> remainder_str = "C";
+                        case 13 -> remainder_str = "D";
+                        case 14 -> remainder_str = "E";
+                        case 15 -> remainder_str = "F";
+                    }
+                }
+                out_str.append(remainder_str);
+                break;
+            } else if (quotient < 16) break;
+        }
+        return out_str.reverse().toString();
+    }
+
+    public static String BinToDecimal(String str){
+
+
+
+
+    }
+
 }
+
+
+
