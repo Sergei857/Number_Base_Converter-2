3aa73e72dd490fe78935b2d4669074a87870cf75
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2023-01-01 23:29:28 +0300

    5.0

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -1,24 +1,22 @@
 package converter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
     static StringBuilder CHARE_BASE = new StringBuilder("0123456789abcdefghijklmnopqrstuvwxyz");
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Enter two numbers in format: {source base} {target base} (To quit type /exit) ");
-            String type_to = InputStr();
+            String type_to = scanner.nextLine();
             if (type_to.equals("/exit")) break;
             int source_base = Integer.parseInt(type_to.split("\\s+")[0]), target_base = Integer.parseInt(type_to.split("\\s+")[1]);
-            System.out.println(source_base + " " + target_base);
             while (true) {
                 System.out.print("Enter number in base %d  to convert to base %d (To go back type /back) "
                         .formatted(source_base, target_base));
-                type_to = InputStr();
+                type_to = scanner.nextLine();
                 if (type_to.equals("/back")) break;
                 String num_in_str = new String(type_to);
                 System.out.println("Conversion result: " + ToBase(ToDecimal(num_in_str, source_base), target_base));
@@ -30,7 +28,8 @@ public class Main {
         BigInteger dec_out = BigInteger.ZERO;
         int i = 0;
         for (Character ch : new StringBuilder(num_in_str).reverse().toString().toCharArray()) {
-            dec_out = dec_out.add(BigInteger.valueOf(CHARE_BASE.toString().indexOf(ch)).multiply(new BigInteger(String.valueOf(source_base)).pow(i)));
+            dec_out = dec_out.add(BigInteger.valueOf(CHARE_BASE.toString().indexOf(ch)).multiply
+                    (new BigInteger(String.valueOf(source_base)).pow(i)));
             i++;
         }
         return dec_out.toString();
@@ -46,11 +45,6 @@ public class Main {
             base_out.append(CHARE_BASE.charAt(ostatok.intValue()));
         }
         return base_out.reverse().toString();
-    }
-
-    public static String InputStr() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        return reader.readLine();
     }
 }
 
