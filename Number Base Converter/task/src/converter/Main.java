c4601102c14478b3a912f1657ff54c938b5af800
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2023-01-02 13:31:26 +0300

    5.end

--- Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/task/src/converter/Main.java
@@ -26,41 +26,12 @@ public class Main {
                     System.out.println("Conversion result: " + ToBase(ToDecimal(num_in_str.toString(), source_base), target_base));
                 } else {
                     StringBuilder celay = new StringBuilder(num_in_str.substring(0, num_in_str.indexOf(".")));
-                    System.out.println(celay);
                     StringBuilder drobnay = new StringBuilder(num_in_str.substring(num_in_str.indexOf(".") + 1, num_in_str.length()));
-                    System.out.println(drobnay);
-                    System.out.println("Conversion result: CELAY   -> " + ToBase(ToDecimal(celay.toString(), source_base), target_base));
-                    System.out.println("Conversion result: DROBNAY-DEC -> " + ToDecimalDrob(drobnay.toString(), source_base));
-                    System.out.println("Conversion result: DROBNAY-BAS -> " + ToBaseDrob(ToDecimalDrob(drobnay.toString(), source_base), target_base));
-
-
+                    System.out.println("Conversion result: " + ToBase(ToDecimal(celay.toString(), source_base), target_base) + "."
+                            + ToBaseDrob(ToDecimalDrob(drobnay.toString(), source_base), target_base).substring(0, 5));
                 }
             }
         }
-    }
-
-    public static String ToBaseDrob(String num_in_str, int target_base) {
-        BigInteger dec_in = new BigInteger(num_in_str);
-        StringBuilder base_out = new StringBuilder("");
-        BigInteger ostatok;
-        while (dec_in.compareTo(BigInteger.ZERO) > 0) {
-            ostatok = dec_in.mod(BigInteger.valueOf(target_base));
-            dec_in = dec_in.subtract(ostatok).divide(BigInteger.valueOf(target_base));
-            base_out.append(CHARE_BASE.charAt(ostatok.intValue()));
-        }
-        return base_out.reverse().toString();
-    }
-
-    public static String ToDecimalDrob(String num_in_str, int source_base) {
-        Double dec_out = 0.0;
-        int i = num_in_str.length();
-        for (Character ch : new StringBuilder(num_in_str).reverse().toString().toCharArray()) {
-            dec_out = dec_out + CHARE_BASE.toString().indexOf(ch) * Math.pow(source_base, -i);
-            i--;
-            System.out.println("Проход" + i + " -> число " + ch + " Результат ->" + dec_out);
-        }
-        BigDecimal big_dec_out = new BigDecimal(dec_out);
-        return new String(String.valueOf(big_dec_out.setScale(5, RoundingMode.HALF_UP)));
     }
 
 
@@ -85,6 +56,32 @@ public class Main {
             base_out.append(CHARE_BASE.charAt(ostatok.intValue()));
         }
         return base_out.reverse().toString();
+    }
+
+    public static String ToDecimalDrob(String num_in_str, int source_base) {
+        Double dec_out = 0.0;
+        int i = num_in_str.length();
+        for (Character ch : new StringBuilder(num_in_str).reverse().toString().toCharArray()) {
+            dec_out = dec_out + CHARE_BASE.toString().indexOf(ch) * Math.pow(source_base, -i);
+            i--;
+        }
+        BigDecimal big_dec_out = new BigDecimal(dec_out);
+        return new String(String.valueOf(big_dec_out.setScale(10, RoundingMode.HALF_UP)));
+    }
+
+    public static String ToBaseDrob(String num_in_str, int target_base) {
+        BigDecimal dec_in = new BigDecimal(num_in_str);
+        StringBuilder base_out = new StringBuilder("");
+        Integer celoe = null;
+        while (true) {
+            celoe = dec_in.multiply(BigDecimal.valueOf(target_base)).setScale(0, RoundingMode.DOWN).intValue();
+            base_out.append(CHARE_BASE.charAt(celoe));
+            if (base_out.length() == 6) {
+                break;
+            }
+            dec_in = dec_in.multiply(BigDecimal.valueOf(target_base)).subtract(BigDecimal.valueOf(celoe));
+        }
+        return base_out.toString()+"00000";
     }
 }
 
